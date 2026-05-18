ce5a397ca562eb31f0435c55cda8e9c7f4346ca7
VIJAYARAJ A N <vijayrajuappal@gmail.com>
2026-05-18 12:36:38 +0530

    CoderabbitFix

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,20 +5,20 @@ import EmployeeDashboard from '../components/EmployeeDashboard'
 import AdminDashboard from '../components/AdminDashboard'
 
 const Dashboard = () => {
-  const [data,setData]=useState(null)
-  const [loading,setLoading]=useState(true)
-  useEffect(()=>{
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
     setData(dummyAdminDashboardData)
-    setTimeout(()=>{setLoading(false)},1000)  
-  },[])
+    setTimeout(() => { setLoading(false) }, 1000)
+  }, [])
 
-  if(loading) return <Loading/>
-  if(!data) return <p className='text-center text-state-500 py-12'>Failed to load dashboard</p>
+  if (loading) return <Loading />
+  if (!data) return <p className='text-center text-slate-500 py-12'>Failed to load dashboard</p>
 
-  if(data.role==="ADMIN"){
-    return <AdminDashboard data={data}/>
-  }else{
-    return <EmployeeDashboard data={data}/>
+  if (data.role === "ADMIN") {
+    return <AdminDashboard data={data} />
+  } else {
+    return <EmployeeDashboard data={data} />
   }
 
 }
